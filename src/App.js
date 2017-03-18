ac88e9f222b1f3e08404d6ee657d5377bb982fb9
Dennis Cortés <hi@cortes.us>
2017-03-17 23:29:23 -0500

    Adds links, adds better description, and changes font sizes

--- src/App.js
+++ src/App.js
@@ -6,54 +6,57 @@ class App extends Component {
     return (
       <div className="ma0 pa0 w-100 animated fadeIn">
         <div className="w-100 h01 bg-rainbow" />
-        <div className="fl mw6 w-100 ph4 pv3">
+        <div className="fl mw6 w-100 ph4 pt3">
           <h1 className="f2 fw4 lh-copy">Hey <span className="absolute spin">👋</span><br />I'm Dennis Cortés.</h1>
-          <h2 className="f5 fw4 lh-copy mb4">Designer, Illustrator, Developer, &amp; Writer.</h2>
-          <h2 className="f5 fw4 lh-copy">New website coming soon, watch me<br />stream my process on <a className="black rainbow-hover" href="https://twitch.tv/cortesarts">Twitch.</a></h2>
-          <p><a href="#" className="rainbow">hi@cortes.us</a></p>
+          <h2 className="f5 fw4 lh-copy mb4">Designer, Illustrator, Developer, &amp; Writer.<br />Lead Designer at Satchel Health,<br />Co-Founder at Quarry.</h2>
+          <h2 className="f6 fw4 lh-copy">New website coming soon, watch me<br />stream my process on <a className="black rainbow-hover" href="https://twitch.tv/cortesarts">Twitch.</a></h2>
+          <p><a href="mailto:hi@cortes.us" className="f6 rainbow">hi@cortes.us</a></p>
           <div id="Work">
             <ul className="list pl0 pv2">
               <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://dribbble.com/fromcortes">
+                Blog
+              </a></li>
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://dribbble.com/fromcortes">
                 Portfolio
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://instagram.com/calligritype">
                 Calligritype
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://coffee.cortes.us">
                 Taste Notes
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://fieldy.co">
                 Fieldy
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://sogol.co">
                 Sogol
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://instagram.com/illustree">
                 Illustree
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://usequarry.com">
                 Quarry
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://soundcloud.com/cordio/tracks">
                 Cordio
               </a></li>
             </ul>
           </div>
           <div id="Social">
-            <ul className="list pl0 pv2">
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+            <ul className="list pl0 pt2">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://twitch.tv/cortesarts">
                 Twitch
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://twitter.com/fromcortes">
                 Twitter
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="http://youtube.com/cortesarts">
                 YouTube
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://instagram.com/fromcortes">
                 Instagram
               </a></li>
-              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="#">
+              <li className="pv1 link lh-title f6"><a className="black-50 rainbow-hover" href="https://github.com/fromcortes">
                 GitHub
               </a></li>
             </ul>
